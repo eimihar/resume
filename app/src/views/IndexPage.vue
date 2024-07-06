affe4b08895b6bfc33420464a147ddd4af440a11
Ahmad Rahimie <newrehmi@gmail.com>
2024-07-06 10:32:23 +0800

    vvvb

--- app/src/views/IndexPage.vue
+++ app/src/views/IndexPage.vue
@@ -11,8 +11,9 @@ export default {
           Rahimie Ahmad
         </div>
         <div>
-          Software Engineer
+          Polyglot Software Engineer
         </div>
+        <div style="opacity: 0.7;">Not actively looking</div>
       </div>
     </div>
     <div class="columns resume-body">
@@ -20,9 +21,9 @@ export default {
         <div class="section-contact">
           <div class="side-title">Links</div>
           <div>newrehmi@gmail.com</div>
-          <div>github.com/eimihar</div>
-          <div>linked.in/eimihar</div>
-          <div>twitter.com/eimihar</div>
+          <div><a href="https://github.com/eimihar" target="_blank">github.com/eimihar</a></div>
+          <div><a href="https://www.linkedin.com/in/eimihar" target="_blank">linked.in/in/eimihar</a></div>
+          <div><a href="https://twitter.com/eimihar" target="_blank">twitter.com/eimihar</a></div>
         </div>
         <div class="section-skills">
           <div class="side-title">Skills</div>
@@ -32,39 +33,63 @@ export default {
               php, js, typescript, mysql
             </div>
             <div>
+              html, css
+            </div>
+            <div class="minor">
               python, c#, node.js
             </div>
+            <div class="minor">
+              dart (flutter), kotlin
+            </div>
             <div class="side-subtitle">frameworks</div>
-            <div>laravel, exedra, vue, quasar, ionic, flutter</div>
+            <div>laravel, exedra, vue, quasar</div>
+            <div class="minor">ionic, flutter</div>
             <div class="side-subtitle">backend</div>
             <div>
-              php, mysql, node.js, lamp, ubuntu, linux, orm, rest APIs, express.js
+              php, mysql, node.js, lamp, ubuntu, linux, orm / active record, rest APIs, express.js
             </div>
             <div class="side-subtitle">architectural</div>
-            <div>design pattern, oop, mvc</div>
+            <div>design pattern, oop, mvc, composite, adapter, IoC, meta / decorators, php PSR, async</div>
             <div class="side-subtitle">frontend</div>
-            <div>html, css, js, jquery, vue, typescript, flutter, ionic, quasar, chrome extension</div>
+            <div>html, css, js, jquery, vue, typescript, flutter, ionic, quasar, bulma, bootstrap</div>
+            <div class="minor">electron, chrome extension</div>
             <div class="side-subtitle">ops</div>
             <div>
-              aws, ec2, rds, s3, ses, bash, system.d, linux, ubuntu, ssh, sftp, vercel, azure
+              vps, aws, ec2, rds, s3, ses, bash, system.d, linux, ubuntu, ssh, sftp, vercel, azure
             </div>
             <div class="side-subtitle">others</div>
             <div>
               openai, llm, raspberry, websocket, pusher.js
             </div>
+            <div class="side-subtitle">tools</div>
+            <div>phpstorm, vscode, notion, asana, trello, github, bitbucket</div>
           </div>
         </div>
       </div>
       <div class="column container-main">
         <div style="font-weight: bold; font-size: 25px;">Work Experience</div>
         <div class="section-experience">
-          <div class="exp-title">Lead Software Developer</div>
-          <div class="exp-company">Queuemed Healthcare Sdn Bhd</div>
-          <div class="exp-duration">November 2019 - current</div>
+          <div class="columns">
+            <div class="column">
+              <div class="exp-title">Lead Software Developer</div>
+              <div class="exp-company">Queuemed Healthcare Sdn Bhd</div>
+              <div class="exp-duration">November 2019 - current</div>
+            </div>
+            <div class="column is-one-fifth">
+              <div class="tabs is-right">
+                <ul>
+                  <li class="is-active"><a>Summary</a></li>
+                  <li><a>Projects</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
           <div class="exp-detail">
             <ul>
               <li>
-                Development of hardware firmware includes scalable devices deployment like queuescreens, ticket printers (scalable as in support/technical experience in setting up additionals for each deployment)
+                Development of hardware firmware includes scalable devices deployment like queuescreens, ticket printers
+                (scalable as in support/technical experience in setting up additionals for each deployment)
               </li>
               <li>
                 Sets up (REST) APIs development (that has covered literal thousands internal endpoints, and hundreds of v1 APIs) and still maintaining and developing another hundreds (that's being utilized by major products (patient apps, dashboards, kiosks, copilot, including thirdparties integration)
@@ -76,13 +101,13 @@ export default {
                 Handles most deployment stuff (except mobile builds), including setting up productions , provisioning and development instances for most projects (aws, linux, ubuntu, s3, ec2, rds, azure vm, vercel etc), env based deployment and et cetera cetera et
               </li>
               <li>
-                Feature development and revamps includes customizable appointment slotting/scheduling design, ticket printer, queuescreens and management, service queues design (websocket etc), patient portal, online ticket, clinics management, ads management
+                Feature development and revamps includes customizable appointment slotting/scheduling design, ticket printer, queuescreens and management, service queues design (websocket etc), patient portal, online ticket, clinics management, ads management, queue sequence and blueprint, feedback system
               </li>
               <li>
                 Codebase revamps to scale for features mostly or how APIs are developed, maintained
               </li>
               <li>
-                Development leads for qmed, copilot project, patient hybrid app revamp, doktoraku, hardware firmwares, qmed go (teleconsultation kiosk), APIs development
+                Development leads for qmed, copilot project, patient hybrid app revamp, doktoraku, hardware firmwares, qmed go (teleconsultation kiosk), APIs development, and couple of HIS/EMR integrations
               </li>
               <li>
                 Development of realtime features from queuescreens, to patient facing frontend, and implementation of websocket, teleconsultation, firmware (raspi, kiosk), openai etc
@@ -133,6 +158,12 @@ export default {
   </div>
 </template>
 <style lang="scss" scoped>
+.section-skills {
+  .minor {
+    opacity: 0.6;
+  }
+}
+
 .container-resume {
   margin-top: 50px;
   background: white;
