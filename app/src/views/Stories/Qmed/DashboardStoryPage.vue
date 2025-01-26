bb861d995859fa35d90c7d5b1b1fc50928217164
Ahmad Rahimie <newrehmi@gmail.com>
2025-01-26 20:29:24 +0800

    Update DashboardStoryPage.vue

--- app/src/views/Stories/Qmed/DashboardStoryPage.vue
+++ app/src/views/Stories/Qmed/DashboardStoryPage.vue
@@ -6,6 +6,9 @@
       <li>Development of access levels on how members access the features</li>
       <li>Development of queues management, from journey sequence, and sequence blueprint, to the general level how how patients usually queue.</li>
       <li>Development of (reworked) appointment and comprehensive schedule slotting system</li>
+      <li>Development of standard managing features from members management, clinic clients / patients, billing, clinic insights, account settings,
+        services, packages, stations up even to vital monitoring
+      </li>
       <li>Development of internal frameworks or standard on how things are developed</li>
     </ul>
   </StoryContentView>
