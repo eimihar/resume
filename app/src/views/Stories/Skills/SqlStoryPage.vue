abd905e869b289412c757e8a2ecdbb94b6678779
Ahmad Rahimie <newrehmi@gmail.com>
2025-01-31 11:51:09 +0800

    Create SqlStoryPage.vue

---
+++ app/src/views/Stories/Skills/SqlStoryPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <StoryTitleView>SQL</StoryTitleView>
+  <StoryContentView>
+    <ul>
+      <li>Have worked with SQL since the beginning of time</li>
+      <li>Knows basic to intermediate queries on how to fetch things</li>
+      <li>Well with query optimizations especially around composite indexes or figuring out which part of an app is actually caused by bad query</li>
+    </ul>
+  </StoryContentView>
+</template>
+<script setup lang="ts">
+import StoryContentView from "@/views/Stories/StoryContentView.vue";
+import StoryTitleView from "@/views/Stories/StoryTitleView.vue";
+</script>
